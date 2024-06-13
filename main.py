2e97dff6e34b06271a2721dae13ded0fbb58f05f
sameer <sameerecb@gmail.com>
2024-06-13 15:58:57 +0530

    Added description on the new columns.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
+
 with col1:
     st.image("images/photo.jpg")
 
@@ -15,4 +16,6 @@ with col2:
      I currently work on SAP HANA solution for different hardware."""
     st.write(content)
     st.info(content)  # For different appearance
-
+content2 = """Below you can find some of the apps, I have built in python. Feel free to contact me.
+           I am 14 years of experience software engineer."""
+st.write(content2)
