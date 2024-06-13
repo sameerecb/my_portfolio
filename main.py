9966d1e18c2dd1a5be1384c062325a814ef9d315
sameer <sameerecb@gmail.com>
2024-06-13 17:01:09 +0530

    Added app description, images and links

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
@@ -19,3 +20,22 @@ with col2:
 content2 = """Below you can find some of the apps, I have built in python. Feel free to contact me.
            I am 14 years of experience software engineer."""
 st.write(content2)
+
+col3, empty_col,  col4 = st.columns([1.5, .5, 1.5])
+
+df = pandas.read_csv("data.csv")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        # st.write("[Source Code](https://pythonhow.com)") Static way to provide a link
+        st.write(f"[Source Code]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
