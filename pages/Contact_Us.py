44c9daa2a2593a91c039bbab8b44ade1b0944d3e
sameer <sameerecb@gmail.com>
2024-06-14 18:06:46 +0530

    Send email using the web form.

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,8 +1,19 @@
 import streamlit as st
+from send_email import send_email
+
+
 st.header("Contact Me")
 with st.form("key=email_form"):
     user_email = st.text_input("Enter your email address")
-    message = st.text_area("Enter your message below")
+    raw_message = st.text_area("Enter your message below")
+    message = f"""\
+Subject: New email from {user_email}
+
+From: {user_email}
+{raw_message}
+"""
     button = st.form_submit_button("Send")
+    print(button)
     if button:
-        print("Pressed")
+        send_email(message)
+        st.info("Your email is sent successfully.")
--- send_email.py
+++ send_email.py
@@ -2,20 +2,16 @@
 import smtplib
 import ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "theflexfound@gmail.com"
-password = "cekfmnjqvsfuohbf"
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
 
-receiver = "awizomfashion@gmail.com"
-message = """\
-Subject: Hi!
-Hello , 
-how are you. 
-Bye !"""
+    username = "theflexfound@gmail.com"
+    password = "cekfmnjqvsfuohbf"
 
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message )
+    receiver = "awizomfashion@gmail.com"
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
